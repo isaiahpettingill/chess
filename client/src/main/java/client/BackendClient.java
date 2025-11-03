28b0aca11ba3686db6b5cf32b00e150c2194a6c0
isaiahpettingill <isaiahjp@byu.edu>
2025-11-03 10:36:47 -0700

    server facade created

--- client/src/main/java/client/BackendClient.java
+++ client/src/main/java/client/BackendClient.java
@@ -1,4 +1,5 @@
 package client;
+
 import dto.*;
 
 import java.io.IOException;
@@ -17,20 +18,19 @@ public class BackendClient {
     final static Gson gson = new Gson();
 
     private Optional<String> auth;
-    private String getAuth(){
+
+    private String getAuth() {
         return auth.isPresent() ? auth.get() : "";
     }
-
 
     public BackendClient(String baseurl) {
         this.baseurl = baseurl;
     }
 
-    private static URI makeUri(String base, String path){
+    private static URI makeUri(String base, String path) {
         try {
             return new URI(base + path);
-        }
-        catch (Exception ex){
+        } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
     }
@@ -38,31 +38,79 @@ public class BackendClient {
     public ServerResponse<RegisterResponse> register(RegisterPayload payload) throws IOException, InterruptedException {
         var req = HttpRequest.newBuilder(makeUri(baseurl, "user"))
                 .POST(BodyPublishers.ofString(gson.toJson(payload)))
-                .header("Authorization", getAuth())
                 .build();
+        var res = http.send(req, BodyHandlers.ofString());
+        var status = res.statusCode();
+        var body = status == 200 ? gson.fromJson(res.body(), RegisterResponse.class) : null;
+        if (status == 200 && body != null) {
+            auth = Optional.of(body.authToken());
+        }
+        return new ServerResponse<>(body, status);
     }
 
     public ServerResponse<LoginResponse> login(LoginPayload payload) throws IOException, InterruptedException {
-    
+        var req = HttpRequest.newBuilder(makeUri(baseurl, "session"))
+                .POST(BodyPublishers.ofString(gson.toJson(payload)))
+                .build();
+        var res = http.send(req, BodyHandlers.ofString());
+        var status = res.statusCode();
+        var body = status == 200 ? gson.fromJson(res.body(), LoginResponse.class) : null;
+        if (status == 200 && body != null) {
+            auth = Optional.of(body.authToken());
+        }
+        return new ServerResponse<>(body, status);
     }
 
     public ServerResponse<Void> logout() throws IOException, InterruptedException {
-        auth = Optional.empty();
+        var req = HttpRequest.newBuilder(makeUri(baseurl, "session"))
+                .DELETE()
+                .header("Authorization", getAuth())
+                .build();
+        var res = http.send(req, BodyHandlers.ofString());
+        var status = res.statusCode();
+        return new ServerResponse<>(null, status);
     }
 
     public ServerResponse<Void> joinGame(JoinGamePayload payload) throws IOException, InterruptedException {
-
+        var req = HttpRequest.newBuilder(makeUri(baseurl, "game"))
+                .PUT(BodyPublishers.ofString(gson.toJson(payload)))
+                .header("Authorization", getAuth())
+                .build();
+        var res = http.send(req, BodyHandlers.ofString());
+        var status = res.statusCode();
+        return new ServerResponse<>(null, status);
     }
 
-    public ServerResponse<CreateGameResponse> createGame(CreateGamePayload payload) throws IOException, InterruptedException {
+    public ServerResponse<CreateGameResponse> createGame(CreateGamePayload payload)
+            throws IOException, InterruptedException {
+        var req = HttpRequest.newBuilder(makeUri(baseurl, "game"))
+                .POST(BodyPublishers.ofString(gson.toJson(payload)))
+                .header("Authorization", getAuth())
+                .build();
+        var res = http.send(req, BodyHandlers.ofString());
+        var status = res.statusCode();
+        var body = status == 200 ? gson.fromJson(res.body(), CreateGameResponse.class) : null;
+        return new ServerResponse<>(body, status);
 
     }
 
     public ServerResponse<Void> clearDb() throws IOException, InterruptedException {
-
+        var req = HttpRequest.newBuilder(makeUri(baseurl, "db"))
+                .DELETE()
+                .build();
+        var res = http.send(req, BodyHandlers.ofString());
+        var status = res.statusCode();
+        return new ServerResponse<>(null, status);
     }
 
     public ServerResponse<ListGamesResponse> listGames() throws IOException, InterruptedException {
-
+        var req = HttpRequest.newBuilder(makeUri(baseurl, "game"))
+                .GET()
+                .header("Authorization", getAuth())
+                .build();
+        var res = http.send(req, BodyHandlers.ofString());
+        var status = res.statusCode();
+        var body = status == 200 ? gson.fromJson(res.body(), ListGamesResponse.class) : null;
+        return new ServerResponse<>(body, status);
     }
 }
