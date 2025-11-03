d81d2d8dde994e51580a28c2885c3f321e1db3f0
isaiahpettingill <isaiahjp@byu.edu>
2025-11-03 10:30:46 -0700

    some crap yall

--- client/src/main/java/client/BackendClient.java
+++ client/src/main/java/client/BackendClient.java
@@ -1,10 +1,12 @@
 package client;
 import dto.*;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.util.Optional;
 
 import com.google.gson.Gson;
@@ -14,7 +16,11 @@ public class BackendClient {
     final static HttpClient http = HttpClient.newHttpClient();
     final static Gson gson = new Gson();
 
-    private Optional<String> authentication;
+    private Optional<String> auth;
+    private String getAuth(){
+        return auth.isPresent() ? auth.get() : "";
+    }
+
 
     public BackendClient(String baseurl) {
         this.baseurl = baseurl;
@@ -29,34 +35,34 @@ public class BackendClient {
         }
     }
 
-    public ServerResponse<RegisterResponse> register(RegisterPayload payload){
+    public ServerResponse<RegisterResponse> register(RegisterPayload payload) throws IOException, InterruptedException {
         var req = HttpRequest.newBuilder(makeUri(baseurl, "user"))
                 .POST(BodyPublishers.ofString(gson.toJson(payload)))
+                .header("Authorization", getAuth())
                 .build();
-        
     }
 
-    public ServerResponse<LoginResponse> login(LoginPayload payload){
+    public ServerResponse<LoginResponse> login(LoginPayload payload) throws IOException, InterruptedException {
     
     }
 
-    public ServerResponse<Void> logout(){
+    public ServerResponse<Void> logout() throws IOException, InterruptedException {
+        auth = Optional.empty();
+    }
+
+    public ServerResponse<Void> joinGame(JoinGamePayload payload) throws IOException, InterruptedException {
 
     }
 
-    public ServerResponse<Void> joinGame(JoinGamePayload payload){
+    public ServerResponse<CreateGameResponse> createGame(CreateGamePayload payload) throws IOException, InterruptedException {
 
     }
 
-    public ServerResponse<CreateGameResponse> createGame(CreateGamePayload payload){
+    public ServerResponse<Void> clearDb() throws IOException, InterruptedException {
 
     }
 
-    public ServerResponse<Void> clearDb(){
-
-    }
-
-    public ServerResponse<ListGamesResponse> listGames(){
+    public ServerResponse<ListGamesResponse> listGames() throws IOException, InterruptedException {
 
     }
 }
