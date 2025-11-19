43e0596c2240234225b17541314271f114337fef
isaiahpettingill <isaiahjp@byu.edu>
2025-11-19 08:28:58 -0700

    committment

--- client/src/main/java/InGameLoop.java
+++ client/src/main/java/InGameLoop.java
@@ -1,6 +1,7 @@
 import java.io.Console;
 
 import client.ServerFacade;
+import jakarta.websocket.Session;
 
 public class InGameLoop {
     private static final Console CONSOLE = System.console();
@@ -11,27 +12,70 @@ public class InGameLoop {
         backend = butt;
     }
 
-    public void loop() {
+    public void move(Session connection) {
+
+    }
+
+    public void resign(Session connection) {
+
+    }
+
+    public void previewMove(Session connection) {
+
+    }
+
+    public void leave(Session connection) {
+
+    }
+
+    public void observe() {
         try {
             final var connection = backend.connectToWebSocket();
             var shouldQuit = false;
-            do {
+            loop: do {
+                CONSOLE.printf("[l] leave\t[p]: preview move\n");
+                final var action = CONSOLE.readLine("ACTION: ");
+                switch (action.toLowerCase()) {
+                    case "l":
+                        leave(connection);
+                        break loop;
+                    case "p":
+                        previewMove(connection);
+                        break;
+                    default:
+                        CONSOLE.printf("Invalid Action\n");
+                        continue loop;
+                }
+            } while (!shouldQuit);
+            connection.close();
+        } catch (Exception ex) {
+            CONSOLE.printf("Something went terribly wrong!\n");
+        }
+    }
+
+    public void play() {
+        try {
+            final var connection = backend.connectToWebSocket();
+            var shouldQuit = false;
+            loop: do {
                 CONSOLE.printf("[m]: move\t[r]: resign\t[l] leave\t[p]: preview move\n");
                 final var action = CONSOLE.readLine("ACTION: ");
                 switch (action.toLowerCase()) {
                     case "m":
+                        move(connection);
                         break;
                     case "r":
-                        shouldQuit = true;
-                        break;
+                        resign(connection);
+                        break loop;
                     case "l":
-                        shouldQuit = true;
-                        break;
+                        leave(connection);
+                        break loop;
                     case "p":
+                        previewMove(connection);
                         break;
                     default:
                         CONSOLE.printf("Invalid Action\n");
-                        break;
+                        continue loop;
                 }
             } while (!shouldQuit);
             connection.close();
